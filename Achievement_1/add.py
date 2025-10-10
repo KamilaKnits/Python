58941ede7e310ef88644b064904f8d60483d7456
KamilaKnits <103442012+KamilaKnits@users.noreply.github.com>
2025-10-10 14:37:25 -0400

    exercise_2.2

--- Achievement_1/add.py
+++ Achievement_1/add.py
@@ -1,4 +1,4 @@
 a = int(input("Enter a number: "))
 b = int(input("Enter another number to be added to the first: "))
 
-print("The sum of there numbers is" + str(a + b))
+print("The sum of there numbers is " + str(a + b))
